be2d0fc731f307f07ce409f6af047735bdf1f0d3
Chris Foster <chris.foster@techdept.co.uk>
2017-03-23 08:24:10 +0000

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+     new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+}
